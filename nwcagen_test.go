1b05728e3bb5904c8179e3e9bb8f40343981a87b
cubicdaiya <cubicdaiya@gmail.com>
2014-07-10 17:51:04 +0900

    add test

---
+++ nwcagen_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_affinityEven(t *testing.T) {
+	actual := "worker_cpu_affinity 1000 0100 0010 0001;"
+	expected := affinityEven(4)
+	if actual != expected {
+		t.Errorf("got %s\nwant %s", actual, expected)
+	}
+}
